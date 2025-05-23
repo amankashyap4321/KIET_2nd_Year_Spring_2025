3a02cb42c64bea305840628573dfe88c4f4bfcbd
codewithrahuldixit <140512374+codewithrahuldixit@users.noreply.github.com>
2025-05-23 15:45:36 +0530

    AI B and AIML A 23:05:2025 Lecture 01

---
+++ AI_2B/src/p1/Circle.java
@@ -0,0 +1,29 @@
+package p1;
+
+public class Circle implements Shape {
+	private int radius;
+
+	public Circle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}
+
+	public Circle(int radius) {
+		super();
+		this.radius = radius;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+}
---
+++ AI_2B/src/p1/DrawingDemo.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class DrawingDemo {
+	public static void main(String[] args) {
+		Circle c = new Circle(10);
+		Rectangle r = new Rectangle(10,20);
+		DrawingApp d = new DrawingApp(r);
+		d.draw();
+	}
+}
