5bd8165e01ad4369a246ac18de14f0b9aa5d9a44
TeachToTech-in <posttoteachtotech@gmail.com>
2025-06-03 15:57:05 +0530

    AI2A & AIML C & AIB 03:06:2025

--- AI_2A_FirstRestAPI/src/main/java/com/kiet/AI_2A_FirstRestAPI/controller/StudentController.java
+++ AI_2A_FirstRestAPI/src/main/java/com/kiet/AI_2A_FirstRestAPI/controller/StudentController.java
@@ -2,11 +2,15 @@ package com.kiet.AI_2A_FirstRestAPI.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kiet.AI_2A_FirstRestAPI.StudentService;
 import com.kiet.AI_2A_FirstRestAPI.model.Student;
+import com.kiet.AI_2A_FirstRestAPI.service.StudentService;
 
 @RestController
 public class StudentController {
@@ -20,9 +24,35 @@ public class StudentController {
 	}
 
 
-	@RequestMapping("/students")
+	@GetMapping("/students")
 	//toreturn All The students
 	public List<Student> getAllStudents() {
 		return service.reteriveAllStudents();
 	}
+	
+	//http://localhost:9090/students/101
+	@GetMapping("/students/{id}")
+	//toreturn All The students
+	public Student getStudentById(@PathVariable int id ) {
+		return service.reteriveStudentById(id);
+	}
+	
+	@DeleteMapping("/students/{id}")
+	//toreturn All The students
+	public void removeStudent(@PathVariable int id ) {
+		service.removeById(id);
+	}
+	
+	
+
+	@PostMapping("/students")
+	//toreturn All The students
+	public Student insertStudent(@RequestBody Student student) {
+		return service.saveStudent(student);
+	}
+	
+	
+	
+	
+	
 }
